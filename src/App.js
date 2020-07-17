5613fcf98e3aa2dde151bbca49aeaf789d829f52
mzulya <sul.zulya@gmail.com>
2020-07-17 12:24:50 +0700

    add numbers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { count, length } from ‘./numbers’;
+import { count, length } from './numbers';
 
 const value = 'i am here';
 const num = 28;
