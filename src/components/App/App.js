ed38443249d59c20aacf53f1d947fcb229948702
mzulya <sul.zulya@gmail.com>
2020-07-20 11:21:02 +0700

    keys

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,11 +5,24 @@ import ItemList from '../ItemList/ItemList';
 
 const todoItem = 'Закончить React';
 
-const App = () => (
-	<div>
-		<h1>Важные дела:</h1>
-		<ItemList todoItem={todoItem}/>
-		<Footer count={3}/>
-	</div>);
+const App = () => 
+  const items = [
+    {
+      value: 'Закончить React'
+    },
+    {
+      value: 'Закончить портфолио'
+    },
+    {
+      value: 'Найти работу'
+    }
+  ];
 
-	export default App;
+  return (
+  <div>
+    <h1>Важные дела:</h1>
+    <ItemList items={items}/>
+    <Footer count={3}/>
+  </div>);
+
+export default App;
