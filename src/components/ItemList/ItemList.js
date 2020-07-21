465e7f44c025c1723092e44d7345c5073ef4bc50
mzulya <sul.zulya@gmail.com>
2020-07-21 19:31:17 +0700

    add styles

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -2,25 +2,16 @@ import React from 'react';
 import Item from '../Item/Item';
 import styles from './ItemList.module.css';
 import Checkbox from '@material-ui/core/Checkbox';
-
-const Checkboxes = () => {
-  const [checked, setChecked] = React.useState(true);
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked(event.target.checked);
-  };
+import DeleteRoundedIcon from '@material-ui/icons/DeleteRounded';
 
 const ItemList =({items}) => (<ul>
   {items.map(item =>
     <li className ={styles.list} key={item.value}>
-    <Checkbox
-        checked={checked}
-        onChange={handleChange}
-        inputProps={{ 'aria-label': 'primary checkbox' }}
-    />
+    <Checkbox />
     <Item
         value={item.value}
         isDone={item.isDone}/>
+      <DeleteRoundedIcon />
     </li>)}
 </ul>);
 
