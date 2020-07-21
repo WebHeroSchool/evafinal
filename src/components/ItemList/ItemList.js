11ce506e55e74645ecdd8fb6cb86d8632d626ffe
mzulya <sul.zulya@gmail.com>
2020-07-21 20:06:39 +0700

    add styles

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -2,16 +2,20 @@ import React from 'react';
 import Item from '../Item/Item';
 import styles from './ItemList.module.css';
 import Checkbox from '@material-ui/core/Checkbox';
-import DeleteRoundedIcon from '@material-ui/icons/DeleteRounded';
+import DeleteTwoToneIcon from '@material-ui/icons/DeleteTwoTone';
 
 const ItemList =({items}) => (<ul>
   {items.map(item =>
     <li className ={styles.list} key={item.value}>
+    <div className = {styles.check}>
     <Checkbox />
+    </div>
     <Item
         value={item.value}
         isDone={item.isDone}/>
-      <DeleteRoundedIcon />
+    <div className ={styles.delete}>
+    <DeleteTwoToneIcon />
+    </div>
     </li>)}
 </ul>);
 
