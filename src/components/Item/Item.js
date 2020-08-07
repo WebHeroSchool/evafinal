c227920a7340ebbf827d153e5a5e71c1a1bb56c8
mzulya <sul.zulya@gmail.com>
2020-08-07 13:16:56 +0700

    add lifecycle

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -5,17 +5,29 @@ import styles from './Item.module.css';
 
 
 class Item extends React.Component {
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+
   render() {
     const {value, isDone} = this.props;
     return (<span 
-  className={
-  classnames({
-    [styles.item]: true,
-    [styles.done]: isDone
-  })
-}>
-  {value}
-  </span>);
+      className={
+        classnames({
+          [styles.item]: true,
+          [styles.done]: isDone
+        })
+    }>
+    {value}
+    </span>);
   }
 };
 
