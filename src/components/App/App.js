18e0d84ea4958ec7ed89fa7f4b0950a34502da63
mzulya <sul.zulya@gmail.com>
2020-08-08 18:27:40 +0700

    add router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,84 +1,31 @@
 import React, {useState, useEffect} from 'react';
-import ReactDOM from 'react-dom';
-import Footer from '../Footer/Footer';
-import ItemList from '../ItemList/ItemList';
-import InputItem from '../InputItem/InputItem';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import MenuList from '@material-ui/core/MenuList';
+
 import styles from './App.module.css';
 
-function App() {
-  const initialState = {
-    items: [
-      {
-        value: 'Закончить React',
-        isDone: true,
-        id: 1
-      },
-      {
-        value: 'Закончить портфолио',
-        isDone: false,
-        id: 2
-      },
-      {
-        value: 'Найти работу',
-        isDone: false,
-        id: 3
-      }
-    ],
-    count: 3
-  };
-  const [items, setItems] = useState(initialState.items);
-  const [count, setCount] = useState(initialState.count);
+import todo from '../todo/todo';
+import About from '../About/About';
+import Contacts from '../Contacs/Contacs';
 
-  useEffect(() => {
-    console.log('update');
-  });
-
-  useEffect(() => {
-    console.log('mount');
-  }, []);
-
-  const onClickDone = id => {
-    const newItemList = items.map(item => {
-      const newItem = { ...item};
-      if (item.id ===id){
-        newItem.isDone = !item.isDone;
-      }
-      return newItem;
-    });
-
-    setItems(newItemList);
-  };
-
-  const onClickDelete = id => {
-    const newList = items.filter(item => item.id !== id);
-    setItems(newList);
-    setCount(count => count -1);
-  };
-
-  const onClickAdd = value => {
-    const newItems = [
-      ...items,
-      {
-        value,
-        isDone: false,
-        id: count + 1
-      }
-    ];
-    setItems(newItems);
-    setCount(count => count +1);
-  };
-
-  return (
+function App = () =>
+  (<Router>
     <div className = {styles.wrap}>
-      <h1 className = {styles.title}>Важные дела:</h1>
-      <InputItem onClickAdd={onClickAdd}/>
-      <ItemList
-        items={items}
-        onClickDone={onClickDone}
-        onClickDelete={onClickDelete}
-      />
-      <Footer count={count}/>
-    </div>);
-};
+      <div className={styles.sidebar}>
+        <MenuList>
+          <Link to='/'><MenuItem>About</MenuItem></Link>
+          <Link to='/todo'><MenuItem>To do</MenuItem></Link>
+          <Link to='/contacts'><MenuItem>Contacs</MenuItem></Link>
+        </MenuList>
+      </div>
+
+      <div className={styles.content}>
+        <Route path='/' exact component={About} />
+        <Route path='/todo' component={todo} />
+        <Route path='/contacts' component={Contacs} />
+      </div>
+    </div>
+  </Router>);
+
 
 export default App;
---
+++ src/components/todo/todo.js
@@ -0,0 +1,83 @@
+import React, {useState, useEffect} from 'react';
+import ReactDOM from 'react-dom';
+import Footer from '../Footer/Footer';
+import ItemList from '../ItemList/ItemList';
+import InputItem from '../InputItem/InputItem';
+import styles from './Todo.module.css';
+
+function Todo() {
+  const initialState = {
+    items: [
+      {
+        value: 'Закончить React',
+        isDone: true,
+        id: 1
+      },
+      {
+        value: 'Закончить портфолио',
+        isDone: false,
+        id: 2
+      },
+      {
+        value: 'Найти работу',
+        isDone: false,
+        id: 3
+      }
+    ],
+    count: 3
+  };
+  const [items, setItems] = useState(initialState.items);
+  const [count, setCount] = useState(initialState.count);
+
+  useEffect(() => {
+    console.log('update');
+  });
+
+  useEffect(() => {
+    console.log('mount');
+  }, []);
+
+  const onClickDone = id => {
+    const newItemList = items.map(item => {
+      const newItem = { ...item};
+      if (item.id ===id){
+        newItem.isDone = !item.isDone;
+      }
+      return newItem;
+    });
+
+    setItems(newItemList);
+  };
+
+  const onClickDelete = id => {
+    const newList = items.filter(item => item.id !== id);
+    setItems(newList);
+    setCount(count => count -1);
+  };
+
+  const onClickAdd = value => {
+    const newItems = [
+      ...items,
+      {
+        value,
+        isDone: false,
+        id: count + 1
+      }
+    ];
+    setItems(newItems);
+    setCount(count => count +1);
+  };
+
+  return (<div>
+      <h1 className = {styles.title}>Важные дела:</h1>
+      <InputItem onClickAdd={onClickAdd}/>
+      <ItemList
+        items={items}
+        onClickDone={onClickDone}
+        onClickDelete={onClickDelete}
+      />
+      <Footer count={count}/>
+    </div>);
+};
+
+export default Todo;
